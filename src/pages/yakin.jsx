7ffd7ee771283aa9f5967355802186b72f02ea98
Adidtiya Putra Caniago <51223543+Deviantkay@users.noreply.github.com>
2023-06-17 03:15:20 +0700

    Update yakin.jsx

---
+++ src/pages/yakin.jsx
@@ -0,0 +1,28 @@
+const Yakin = () => {
+    return (
+        <>
+        {/*==================== PEYAKINAN ====================*/}
+        <section className="yakin section" id="peyakinan">
+          <div className="yakinbg" />
+          <div className="home__container container grid">
+            <div className="home__data">
+              <h1 className="yakin__data-title center">
+                Masih Ragu Untuk Menggunakan <b>Re-Cycler?</b>
+              </h1>
+              <h3 className="yakin__data-subtitle">
+                Dengan fitur Bank Sampah kekinian ini, kamu bisa berkontribusi
+                dalam menjaga lingkungan sekaligus mendapatkan manfaat
+                finansial. Mulailah beraksi untuk masa depan yang lebih baik
+                bersama Re-Cycler!, Yakin masih Ragu?
+              </h3>
+              <a href="#" className="button-center">
+                Setor Sampahmu
+              </a>
+            </div>
+          </div>
+        </section>
+        </>
+    )
+}
+
+export default Yakin;
