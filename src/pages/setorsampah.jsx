7621e511ff81b5d4684ade045181131a6b337317
DeviantKay <adidtyaputra@gmail.com>
2023-06-17 13:15:29 +0700

    code 2 setor sampah

--- src/pages/setorsampah.jsx
+++ src/pages/setorsampah.jsx
@@ -19,7 +19,62 @@ function setorSampah() {
   }
   return (
       <main className="main">
-        
+        {/*==================== HOME ====================*/}
+        <section className="home section" id="home">
+          <div className="home__vidcontainer">
+            <img
+              src={Frame_2012}
+              alt=""
+              className="home__vid"
+            />
+            <div className="home__overlay" />
+          </div>
+          <div className="ss-bagian1">
+            <img
+              src={Large_Image}
+              alt="Setor Sampah"
+              width="600px"
+              style={{ marginLeft: "-50px" }}
+            />
+            <div>
+              <div style={{ margin: 0 }}>
+                <p className="form-title">Re-Cycler</p>
+                <p className="form-subtitle">
+                  Selamat datang di sistem Re-Cycler, Pengguna. Silahkan
+                  Masukkan data sesuai kebutuhan kamu!
+                </p>
+              </div>
+              <div className="form">
+                <select name="Jenis Sampah">
+                  <option disabled selected>
+                    Masukkan Jenis Sampah
+                  </option>
+                  <option value="daurUlang">Sampah Daur Ulang</option>
+                  <option value="dapur">Sampah Dapur</option>
+                  <option value="barangBekas">Sampah Barang Bekas</option>
+                  <option value="furniture">Sampah Furniture</option>
+                  <option value="puing">Sampah Puing</option>
+                  <option value="alam">Sampah Alam</option>
+                </select>
+                <input type="text" placeholder="Sampah Apa Yang Mau Disetor?" />
+                <select name="Metode Setor Sampah">
+                  <option disabled selected>
+                    Masukkan Metode Setor Sampah
+                  </option>
+                  <option value="jemput">Pick Up By Courrier</option>
+                  <option value="setor">Drop To Drop Point</option>
+                </select>
+                <button className="buttonSetorSampah" type="submit" onClick={Handlersampah}>
+                  Setor
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
+        <Mission />
+        <Eduvid />
+        <Yakin />
+        <Sponsor/>  
       </main>
   );
 }
