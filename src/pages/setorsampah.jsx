aaeefde8477296a3d80854ea96c5071f7f128714
DeviantKay <adidtyaputra@gmail.com>
2023-06-17 13:11:10 +0700

    code 1 setor sampah

---
+++ src/pages/setorsampah.jsx
@@ -0,0 +1,27 @@
+import Frame_2012 from './../assets/img/Frame 2012.png';
+import Large_Image from './../assets/img/Large Image.png';
+import image_16 from './../assets/img/image-16.png';
+import pngtreegarbage_can_with_waste_vector7346990_1 from './../assets/img/pngtreegarbage-can-with-waste-vector7346990-1.png';
+import image_17 from './../assets/img/image-17.png';
+import image_18 from './../assets/img/image-18.png';
+import Group_127 from './../assets/img/Group 127.png';
+import image_19 from './../assets/img/image-19.png';
+import Eduvid from './eduvid';
+import Sponsor from './sponsor';
+import Yakin from './yakin';
+import Mission from './mission';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function setorSampah() {
+  const Handlersampah = () => {
+    toast.success('Your transaction has been processed', { position: toast.POSITION.TOP_CENTER });
+  }
+  return (
+      <main className="main">
+        
+      </main>
+  );
+}
+
+export default setorSampah;
