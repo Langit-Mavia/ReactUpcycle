8335376c216bafc595a33c5e1876e0deeea7d1e3
DeviantKay <adidtyaputra@gmail.com>
2023-06-17 11:35:23 +0700

    kode 2

--- src/pages/eduvid.jsx
+++ src/pages/eduvid.jsx
@@ -24,7 +24,31 @@ const Eduvid = () => {
     };
     return (
         <section className="video section">
-
+            <h2 className="section__title">Video Tour</h2>
+            <div className="video__container container">
+            <p className="video__description">
+                Tekan Tombol Play Untuk Melihat Keindahan Hutan Yang Tidak Tercemar
+            </p>
+            <div className="video__content">
+                <video id="video-file" ref={videoRef} onEnded={handleVideoEnd}>
+                <source
+                    src={video_1}
+                    type="video/mp4"
+                />
+                </video>
+                <button
+                onClick={playPause}
+                className="button button--flex video__button"
+                id="video-button"
+                >
+                {isPlaying ? (
+                    <PauseLineIcon size={25} className="ri-play-line video__button-icon" />
+                ) : (
+                    <PlayLineIcon size={25} className="ri-play-line video__button-icon" />
+                )}
+                </button>
+            </div>
+            </div>
         </section>
     );
 };
